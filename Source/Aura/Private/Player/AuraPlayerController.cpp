437716654db54b4ab462ea1a0f28ff953a496742
hassaanhaq12-Dev <hassaanrajput12@gmail.com>
2025-10-11 23:17:09 +0500

    Initial Project Files

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -103,8 +103,12 @@ void AAuraPlayerController::AbilityInputTagReleased(FGameplayTag InputTag)
 				{
 					Spline->AddSplinePoint(PointLoc, ESplineCoordinateSpace::World);
 				}
+				if (NavPath->PathPoints.Num() > 0)
+				{
 				CachedDestination = NavPath->PathPoints[NavPath->PathPoints.Num() - 1];
-				bAutoRunning = true;
+				bAutoRunning = true;					
+				}
+
 			}
 		}
 		FollowTime = 0.f;
