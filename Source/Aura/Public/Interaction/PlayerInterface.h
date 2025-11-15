3e280fac193ea2ffdb0dcfcb5aab747b63b9c579
hassaanhaq12-Dev <hassaanrajput12@gmail.com>
2025-11-15 17:59:23 +0500

    Initial Project Files

--- Source/Aura/Public/Interaction/PlayerInterface.h
+++ Source/Aura/Public/Interaction/PlayerInterface.h
@@ -40,12 +40,18 @@ public:
 
 	UFUNCTION(BlueprintNativeEvent)
 	void AddToPlayerLevel(int32 InPlayerLevel);
-
+	
 	UFUNCTION(BlueprintNativeEvent)
 	void AddToAttributePoints(int32 InAttributePoints);
-
+	
+	UFUNCTION(BlueprintNativeEvent)
+	int32 GetAttributePoints() const;
+	
 	UFUNCTION(BlueprintNativeEvent)
 	void AddToSpellPoints(int32 InSpellPoints);
+	
+	UFUNCTION(BlueprintNativeEvent)
+	int32 GetSpellPoints() const;
 
 	UFUNCTION(BlueprintNativeEvent)
 	void LevelUp();
